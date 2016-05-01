80044e49367bd60e9b25c1c4d6a91a68c3525166
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-05-01 09:00:02 +0000

    First

---
+++ SQLlikeapig.py
@@ -0,0 +1,202 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# DOCS @ http://mysql-python.sourceforge.net/MySQLdb.html#mysqldb
+import MySQLdb
+import csv
+import gc
+
+class MyPig:
+    def __init__(self):
+        self.host = 'eviate.mysql.pythonanywhere-services.com'
+        self.user = 'eviate'
+        self.passwd = 'Snemeis16'
+        self.name = 'eviate$eviate_main'
+        self.use_unicode = True
+        self.charset = 'utf8'
+
+
+class MyPigTable:
+    def __init__(self, database, tabell):
+        
+        self.db = database
+        self.tabell = tabell
+
+    def open(self):
+        conn = MySQLdb.connect(host = self.db.host,
+                               user = self.db.user,
+                               passwd= self.db.passwd,
+                               db = self.db.name,
+                               use_unicode=self.db.use_unicode, 
+                               charset=self.db.charset)  
+        c = conn.cursor()
+        return c, conn
+
+    def close(self, c, conn):
+        conn.commit()
+        c.close()
+        conn.close()
+        gc.collect()
+
+
+    def format_tostring(self, array):
+        table_s = str(array)
+        table_s = table_s.replace('(','')
+        table_s = table_s.replace(')','')
+        table_s = table_s.replace('\'','')
+
+        return table_s
+
+
+    # ------------------------------------------ #
+    """  FUNKSJONER TIL BESTILLING - SKJEMA  """
+    # ----------------------------------------- #
+
+    def selecttable(self):
+        # --- OPEN, EXECUTE, FETCHALL, CLOSE ---
+        c, conn = self.open()
+        c.execute("SELECT ID, Varegruppe, Merke, Modell, Utsalgspris "
+                  "FROM vareliste") 
+        array = c.fetchall()
+        self.close(c, conn)
+
+        return self.format_tostring(array)
+
+    def searchtable(self, string):
+        # --- OPEN, EXECUTE, FETCHALL, CLOSE ---
+        c, conn = self.open()
+        c.execute("SELECT ID, Varegruppe, Merke, Modell, Utsalgspris "
+                  "FROM vareliste WHERE CONCAT_WS"
+                  "('', ID, Varegruppe, Merke, Modell, Utsalgspris) "
+                  "LIKE '%" + string + "%'")
+        array = c.fetchall()
+        self.close(c, conn)
+      
+        return self.format_tostring(array)
+
+    def selectrow(self, identity):
+        # --- OPEN, EXECUTE, FETCHALL, CLOSE ---
+        c, conn = self.open()
+        c.execute("SELECT ID, Merke, Modell, Utsalgspris FROM vareliste WHERE ID=%s" % identity)
+        array = c.fetchall()
+        self.close(c, conn)
+
+        # ------ FORMAT to STRING -------
+        return array
+
+
+    # ------------------------------------------ #
+    """  FUNKSJONER TIL BESTILLINGSLISTE  """
+    # ----------------------------------------- #
+
+    def selecttable_bestillinger(self):
+        # --- OPEN, EXECUTE, FETCHALL, CLOSE ---
+        c, conn = self.open()
+        c.execute("SELECT ID, Kundenavn, Verdi, Antall, Ny, sist_oppdatert, status "
+                  "FROM bestillinger ORDER BY statnr, ID DESC") 
+        array = c.fetchall()
+        self.close(c, conn)
+
+        # ------ FORMAT to STRING -------
+        #return self.format_tostring(array)
+        return array
+
+
+    def lagre_bestilling(self, ny_bestilling):
+        # nybestilling is a dictionary of [navn, telefon, varer, verdi]
+        # --- OPEN, EXECUTE, FETCHALL, CLOSE ---
+        c, conn = self.open()
+        c.execute("INSERT INTO bestillinger"
+                  "(Kundenavn, Telefon, Varer, Verdi, Antall, Ny, status) "
+                  "VALUES ('%s', '%s', '%s', '%s', '%s', "
+                  "CURRENT_TIMESTAMP(), 'Ny')"
+                   % (ny_bestilling['navn'],
+                  ny_bestilling['telefon'],
+                  ny_bestilling['varer'],
+                  ny_bestilling['verdi'],
+                  ny_bestilling['antall']))
+        self.close(c, conn)
+
+        return 'success'
+
+    def lagre_status(self, ny_status):
+
+        dato_columns = {
+            'Ny': 1,
+            'Bestilt': 2,
+            'Mottatt' : 3,
+            'Levert' : 4  }
+
+        c, conn = self.open()
+        c.execute("UPDATE bestillinger SET status='%s', %s=CURRENT_TIMESTAMP(), "
+                  "statnr=%d WHERE ID='%s'" 
+                   % (ny_status['status'],
+                      ny_status['status'],
+                      dato_columns[ny_status['status']],
+                      ny_status['id']))
+
+        self.close(c, conn)
+        return 'success'
+
+    # ------------------------------------------ #
+    """  FUNKSJONER TIL UPLOAD.html  """
+    # ----------------------------------------- #
+
+    """WORK IN PROGRESS
+
+    def bulk_update(self, file):
+        
+        c, conn = self.connect()
+        c.execute("DELETE FROM %s" % self.tabell)
+
+        self.close(c, conn)
+
+        stop = 10
+        with open(file, newline='') as csvfile:
+            vareliste = csv.reader(csvfile, delimiter=';')
+            # Build SQL query string
+            
+            for row in vareliste:
+                sql_string = "INSERT INTO vareliste VALUES("
+                for cell in row[0:stop]:
+
+                    sql_string += '\'' + cell + '\'' + ',' 
+                sql_string += '\'' + row[stop] + '\'' + ')'
+
+                if row[3] != "":
+                    # Format string
+                    if 'æ' or 'Æ' in sql_string:
+                        sql_string = sql_string.replace('æ', 'ae')
+                        sql_string = sql_string.replace('Æ', 'Ae')
+                    if 'ø' or 'Ø' in sql_string:
+                        sql_string = sql_string.replace('ø','oe')
+                        sql_string = sql_string.replace('Ø','Oe')
+                    if 'å' or 'Å' in sql_string:
+                        sql_string = sql_string.replace('å', 'aa')
+                        sql_string = sql_string.replace('Å', 'Aa')
+
+
+                    # Open, execute , close
+                    c, conn = self.connect()
+                    c.execute(sql_string)
+                    conn.commit()
+
+                    c.close()
+                    conn.close()
+                    gc.collect()
+
+        return "<h1> Vareliste UPDATED</h1>" """
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
---
+++ flask_main.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, session, render_template, \
+                url_for, request, redirect, flash, jsonify
+import gc
+import os
+from SQLlikeapig import MyPig, MyPigTable
+
+app = Flask(__name__)
+# ---- INIT SQL-pigs ---------
+db = MyPig()
+vareliste = MyPigTable(db, 'vareliste')
+best = MyPigTable(db, 'bestillinger')
+
+
+# ---------------- NAV ROUTES -------------------
+
+@app.route('/')
+def index():
+
+    return render_template('login.html')
+
+@app.route('/login', methods=["POST"])
+def login():
+    try:
+        something()
+    except Exception as e:
+        return render_template('login.html', error = e) 
+
+@app.route('/bestliste')
+def bestilling_liste():
+
+    return render_template('/navpages/bestilling_liste.html')
+
+@app.route('/bestskjema')
+def bestilling_skjema():
+
+    return render_template('/navpages/bestilling_skjema.html')
+
+@app.route('/uploadform')
+def uploadform():
+
+    return render_template('/navpages/uploadform.html')
+
+
+# ---------- JAVASCRIPT AJAX ROUTES -------------
+# -------------------------------------------#
+"""    ROUTES FRA BESTILLING - SKJEMA      """
+# -------------------------------------------#
+
+@app.route('/postbestilling', methods=["POST"])
+def lagre_bestilling():
+
+    success = best.lagre_bestilling(request.form)    
+
+    return jsonify(result=success)
+
+@app.route('/getvareutvalg')
+def getvareutvalg():
+    now_string = request.args.get('string')
+    table_string = 'empty'
+    
+    if request.args.get('string'):
+        table_string = vareliste.searchtable(now_string)
+    else:
+        table_string = vareliste.selecttable()
+    
+    return jsonify(liste=table_string)
+
+@app.route('/getvarelinje')
+def getvarelinje():
+
+    linje_id = request.args.get('ID')
+    linje_array = vareliste.selectrow(linje_id)
+
+    return jsonify(linje=linje_array)
+
+@app.route('/getsortcolumn')
+def getsortcolumn():
+    return 0
+
+
+# -------------------------------------------#
+"""    ROUTES FRA BESTILLING - LISTE   """
+# -------------------------------------------#
+
+@app.route('/getbestliste')
+def getbestilling_liste():
+
+    best_array = best.selecttable_bestillinger()
+
+    return jsonify(tabell=best_array)
+
+@app.route('/poststatus', methods=["POST"])
+def lagre_status():
+    try:
+        success = best.lagre_status(request.form)
+        return jsonify(result=success)
+
+    except Exception as e:
+        return jsonify(result= "Server ERROR: " + str(e))
+
+
+
+"""WORK IN PROGRESS
+
+    This is a function that is supposed to run, 
+     when a file is uploaded. 
+      1. Take file. 
+      2. Format file. 
+      3. Delete excisting vareliste in MySql-database
+      4. Import new vareliste to MySql.
+
+@app.route('/vareupdate')
+def update_vareliste():
+    
+    filnavn = request.args.get('fil')
+    vareliste = SQLTable('vareliste')
+
+    success = vareliste.bulk_update('/home/eviate/mysite/uploads/%s.csv' % filnavn)
+    return success"""
+
+
+# set the secret key   
